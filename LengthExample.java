a04966049651459d9df41eb1c5774509b1c86a32
gawademansi2512 <92088715+gawademansi2512@users.noreply.github.com>
2023-01-25 10:34:54 +0530

    Update LengthExample.java

--- LengthExample.java
+++ LengthExample.java
@@ -1,5 +1,5 @@
     public class LengthExample{  
     public static void main(String args[]){  
-    String s1="javatpoint";  
+    String s1="mansi";  
     System.out.println("string length of \""+s1+"\" is: "+s1.length());
     }}  
