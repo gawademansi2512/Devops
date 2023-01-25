6b03ba7c7c870ac6b9e897e870d5042c3896137b
gawademansi2512 <92088715+gawademansi2512@users.noreply.github.com>
2023-01-25 09:53:44 +0530

    Add files via upload

---
+++ LengthExample.java
@@ -0,0 +1,5 @@
+    public class LengthExample{  
+    public static void main(String args[]){  
+    String s1="javatpoint";  
+    System.out.println("string length of \""+s1+"\" is: "+s1.length());
+    }}  
